af8fb0d797f78cd43f773eb0389f67511e996b7e
Justin Merrell <merrelljustin@gmail.com>
2023-02-02 18:44:36 -0500

    fix: reduce forced updates/upgrades

--- django_devops/scripts/continuous_deployment.sh
+++ django_devops/scripts/continuous_deployment.sh
@@ -3,28 +3,21 @@
 dir_path=$1
 
 # ---------------------------------------------------------------------------- #
-#                            System Update/Updgrade                            #
-# ---------------------------------------------------------------------------- #
-
-sudo apt-get update -y && sudo apt-get upgrade -y &
-
-# ---------------------------------------------------------------------------- #
 #                                Update Packages                               #
 # ---------------------------------------------------------------------------- #
+
+git pull --no-edit
 
 # PIP
 python3 -m pip install --upgrade pip
 
-# Django
-python3 -m pip install -U Django
-
 # Install or Update Required Packages
-pip3 install --force-reinstall --upgrade -r requirements.txt
+pip3 install --force-reinstall --upgrade --upgrade-strategy=only-if-needed -r requirements.txt
 
 # Pulls the latest code from GitHub, performs the required steps then deploys.
 
-git pull --no-edit
-python3 "$dir_path"/manage.py collectstatic --noinput --clear
+
+python3 "$dir_path"/manage.py collectstatic --noinput
 python3 "$dir_path"/manage.py migrate
 
 
